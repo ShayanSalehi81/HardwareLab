a5d962663e40b61f7e4ffb22845ca9745b9cb555
Shayan Salehi <79784127+ShayanSalehi81@users.noreply.github.com>
2025-01-01 12:59:13 +0330

    Add Converted Model

---
+++ Benchmark/ModelConverter.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+
+saved_model_dir = 'Benchmark/KeywordSpottingTrainer/trained_models/kws_ref_model.md5'
+converter = tf.lite.TFLiteConverter.from_saved_model(saved_model_dir)
+
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+converter.inference_input_type = tf.int16  # Assuming int16 input
+converter.inference_output_type = tf.int8  # For quantized models
+
+tflite_model = converter.convert()
+
+with open('Benchmark/ConvertedModel/speech_commands_model.tflite', 'wb') as f:
+    f.write(tflite_model)
---
+++ Benchmark/ValidateTFModel.py
@@ -0,0 +1,16 @@
+import numpy as np
+import tensorflow as tf
+
+interpreter = tf.lite.Interpreter(model_path='Benchmark/ConvertedModel/speech_commands_model.tflite')
+interpreter.allocate_tensors()
+
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+dummy_input = np.zeros(input_details[0]['shape'], dtype=np.int16)
+interpreter.set_tensor(input_details[0]['index'], dummy_input)
+
+interpreter.invoke()
+
+output = interpreter.get_tensor(output_details[0]['index'])
+print(output)
